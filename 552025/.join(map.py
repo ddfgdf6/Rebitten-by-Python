82ec6063136db905136e97975d698bcc3d6b2ec9
ddfgdf6 <165582473+ddfgdf6@users.noreply.github.com>
2025-05-14 14:50:58 +0400

    Add files via upload

---
+++ 552025/.join(map.py
@@ -0,0 +1,4 @@
+.join(map(str, my_list))  # Convert each element to a string and join them with a space
+map(str, my_list): This converts each element of the list to a string.
+
+' '.join(...): This joins the string elements of the list with a space ' ' between them
